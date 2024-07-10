02af964de1c13680158aab335563d0dfdb6b52be
Vishal Jadhav <143590501+vishaljadhav7@users.noreply.github.com>
2024-07-10 23:14:12 +0530

    tested with inputs

--- Custom/objectCycleRemove.js
+++ Custom/objectCycleRemove.js
@@ -25,5 +25,26 @@ const removeCycle = (obj) => {
     })(obj);
 }
 
-/
-/ 
+/*
+Input:
+const List = function(val){
+  this.next = null;
+  this.val = val;
+};
+
+const item1 = new List(10);
+const item2 = new List(20);
+const item3 = new List(30);
+
+item1.next = item2;
+item2.next = item3;
+item3.next = item1;
+
+removeCycle(item1);
+console.log(item1);
+
+Output:
+/*
+{val: 10, next: {val: 20, next: {val: 30}}}
+*/
+*/ 
